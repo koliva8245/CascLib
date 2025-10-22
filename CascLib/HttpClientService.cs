506122240c3b52eaea28ee88bae7ae02d284b660
Kevin Oliva <kevin.k.oliva@gmail.com>
2025-10-21 21:01:00 -0500

    removed default httpclient implementation

--- CascLib/HttpClientService.cs
+++ CascLib/HttpClientService.cs
@@ -1,34 +1,20 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-#if NET9_0_OR_GREATER
-using System.Threading;
-#endif
+
+#nullable enable
 
 namespace CASCLib;
 
 internal static class HttpClientService
 {
-    private static HttpClient _httpClient;
-
-#if NET9_0_OR_GREATER
-    private static readonly Lock _lock = new();
-#else
-    private static readonly object _lock = new();
-#endif
+    private static HttpClient? _httpClient;
 
     public static HttpClient Instance
     {
         get
         {
-            if (_httpClient is not null)
-                return _httpClient;
-
-            lock (_lock)
-            {
-                _httpClient ??= CreateDefaultClient();
-            }
+            if (_httpClient is null)
+                throw new InvalidOperationException("HttpClient not set. Please set it via SetHttpClient method before using.");
 
             return _httpClient;
         }
@@ -36,23 +22,6 @@ internal static class HttpClientService
 
     public static void SetHttpClient(HttpClient httpClient)
     {
-        ArgumentNullException.ThrowIfNull(httpClient);
-
         _httpClient ??= httpClient;
-    }
-
-    private static HttpClient CreateDefaultClient()
-    {
-        HttpClient httpClient = new(new HttpClientHandler
-        {
-            AutomaticDecompression = DecompressionMethods.All,
-        })
-        {
-            Timeout = TimeSpan.FromSeconds(30)
-        };
-        httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("CASCLib", "1.0"));
-        httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("(modded fork)"));
-
-        return httpClient;
     }
 }
